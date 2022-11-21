9e0c4ee0cfe72a193035ed6a7bf5b3e4eaa834a5
nataliemezher <89838078+nataliemezher@users.noreply.github.com>
2022-11-21 16:59:23 +0100

    4-SINGLEPOST adds style to singlepost page

--- web/app/themes/midgardstheme/single.php
+++ web/app/themes/midgardstheme/single.php
@@ -1,11 +1,20 @@
 <?php get_header(); ?>
 <div class="single-post-content">
+    <div class="single-post-image">
+        <?php if (!empty(get_the_post_thumbnail())) { ?>
+            <?php the_post_thumbnail('midgards-gallery'); ?>
+        <?php } else { ?>
+            <img src="/app/uploads/placeholder_no_image.png" alt="placeholder for no image found">
+        <?php } ?>
+    </div>
 
-    <?php if (!empty(get_the_post_thumbnail())) { ?>
-        <?php the_post_thumbnail('midgards-gallery'); ?>
-    <?php } else { ?>
-        <img src="/app/uploads/placeholder_no_image.png" alt="placeholder for no image found">
-    <?php } ?>
-    <div class="single-post-text"><?php the_content(); ?></div>
+    <div class="single-post-text">
+        <h2 class="single-post-title">
+            <?php the_title() ?>
+        </h2>
+        <div class="single-post-description">
+            <?php the_content(); ?>
+        </div>
+    </div>
 </div>
 <?php wp_footer(); ?>

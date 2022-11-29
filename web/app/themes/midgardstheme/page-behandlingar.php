e4f6873f2e0123c7beaba417eb0dcfadaf042b86
nataliemezher <nataliemezher96@hotmail.com>
2022-11-29 13:33:56 +0100

    5-BEHANDLING fix structure of page

--- web/app/themes/midgardstheme/page-behandlingar.php
+++ web/app/themes/midgardstheme/page-behandlingar.php
@@ -13,29 +13,28 @@ $result = new WP_Query($post_treatments_query); ?>
     <div class="halsocenter-treatments-hero">
         <?php the_content() ?>
     </div>
-    <div class="treatment-content">
-        <div class="treatment-image">
-            <?php if ($result->have_posts()) : ?>
-                <?php while ($result->have_posts()) : $result->the_post(); ?>
+
+    <?php if ($result->have_posts()) : ?>
+        <?php while ($result->have_posts()) : $result->the_post(); ?>
+            <div class="treatment-content">
+                <div class="treatment-image">
                     <?php if (!empty(get_the_post_thumbnail())) { ?>
                         <?php the_post_thumbnail('midgards-gallery'); ?>
                     <?php } else { ?>
                         <img src="/app/uploads/placeholder_no_image.png" width="150" height="150" alt="placeholder for no image found">
                     <?php } ?>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
-        <div class="treatment-info">
-            <?php if ($result->have_posts()) : ?>
-                <?php while ($result->have_posts()) : $result->the_post(); ?>
+                </div>
+                <div class="treatment-info">
                     <h2 class="treatment-title"><?php the_title(); ?></h2>
                     <div class="treatment-text">
                         <?php the_content() ?>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
-    </div>
+                </div>
+
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
 </div>
 
 <?php get_footer() ?>
